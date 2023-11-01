13688e8264b64a7489b94906e15639169d18f632
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-11-01 10:39:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def process_text(text):
     return prediction, detection, filtered_text
 
 # Aplikasi Streamlit
-st.title("Abusify Streamlit App")
+st.title("AbusifyID")
+st.write("Abusiveness Verification in Bahasa Indonesia.")
 
 # Input teks
 input_text = st.text_area("Masukkan teks:")
@@ -38,24 +39,24 @@ col4.markdown(
 if col1.button("Submit"):
     if input_text:
         prediction, detection, filtered_text = process_text(input_text)
-        st.write("Prediksi Tingkat Abusiveness:", prediction)
-        st.write("Deteksi Kata Kasar:", detection)
-        st.write("Teks Setelah Difilter:", filtered_text)
+        st.info("Abusiveness Level:", prediction)
+        st.info("Abusive Words Detected:", detection)
+        st.info("Abusive Words Filtered:", filtered_text)
 
 # Tombol "Predict"
 if col2.button("Predict"):
     if input_text:
         prediction, _, _ = process_text(input_text)
-        st.write("Prediksi Tingkat Abusiveness:", prediction)
+        st.info("Abusiveness Level:", prediction)
 
 # Tombol "Detect"
 if col3.button("Detect"):
     if input_text:
         _, detection, _ = process_text(input_text)
-        st.write("Deteksi Kata Kasar:", detection)
+        st.info("Abusive Words Detected:", detection)
 
 # Tombol "Filter"
 if col4.button("Filter"):
     if input_text:
         _, _, filtered_text = process_text(input_text)
-        st.write("Teks Setelah Difilter:", filtered_text)
+        st.info("Abusive Words Filtered:", filtered_text)
