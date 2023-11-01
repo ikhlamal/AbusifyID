f81e249a5c5e411553f62bcf58b0f7f593979c3e
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-11-01 10:01:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import streamlit as st
 import abusify_id as ai
 
+# Fungsi untuk mengakses tiga fungsi abusify-id
+def process_text(text):
+    prediction = ai.predict_abusiveness(text)
+    detection = ai.abusiveword_detector(text)
+    filtered_text = ai.abusiveword_filter(text)
+
+    return prediction, detection, filtered_text
+
+# Aplikasi Streamlit
 st.title("Abusify Streamlit App")
 
+# Input teks
 input_text = st.text_area("Masukkan teks:")
 
+# Tombol-tombol
 if st.button("Submit"):
     if input_text:
         prediction, detection, filtered_text = process_text(input_text)
@@ -12,19 +23,6 @@ if st.button("Submit"):
         st.write("Deteksi Kata Kasar:", detection)
         st.write("Teks Setelah Difilter:", filtered_text)
 
-col1, col2, col3, col4 = st.columns(4)
-
-if col1.button("Predict"):
-    if input_text:
-        prediction = ai.predict_abusiveness(input_text)
-        st.write("Prediksi Tingkat Abusiveness:", prediction)
-
-if col2.button("Detect"):
-    if input_text:
-        detection = ai.abusiveword_detector(input_text)
-        st.write("Deteksi Kata Kasar:", detection)
-
-if col3.button("Filter"):
-    if input_text:
-        filtered_text = ai.abusiveword_filter(input_text)
-        st.write("Teks Setelah Difilter:", filtered_text)
+st.button("Predict")
+st.button("Detect")
+st.button("Filter")
