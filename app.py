94f927f2393abbf42b5c4a0f78d694031c04a4a7
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-11-01 09:51:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,31 +15,35 @@ st.title("Abusify Streamlit App")
 # Input teks
 input_text = st.text_area("Masukkan teks:")
 
-# Membuat satu kolom untuk menampilkan tombol secara horizontal
-col1 = st.columns(1)
+# Membuat tiga kolom dengan CSS untuk mengatur tampilan tombol-tombol
+col1, col2, col3, col4 = st.columns(4)
+col1.markdown(
+    f'<style>div.stButton > button {{width: 100%; text-align: center;}}</style>',
+    unsafe_allow_html=True,
+)
 
-# Tombol "Predict"
-if col1.button("Predict"):
-    if input_text:
-        prediction, _, _ = process_text(input_text)
-        st.write("Prediksi Tingkat Abusiveness:", prediction)
-
-# Tombol "Detect"
-if col1.button("Detect"):
-    if input_text:
-        _, detection, _ = process_text(input_text)
-        st.write("Deteksi Kata Kasar:", detection)
-
-# Tombol "Filter"
-if col1.button("Filter"):
-    if input_text:
-        _, _, filtered_text = process_text(input_text)
-        st.write("Teks Setelah Difilter:", filtered_text)
-
-# Tombol "Submit" untuk menampilkan output semua fungsi
+# Tombol "Submit"
 if col1.button("Submit"):
     if input_text:
         prediction, detection, filtered_text = process_text(input_text)
         st.write("Prediksi Tingkat Abusiveness:", prediction)
         st.write("Deteksi Kata Kasar:", detection)
+        st.write("Teks Setelah Difilter:", filtered_text)
+
+# Tombol "Predict"
+if col2.button("Predict"):
+    if input_text:
+        prediction, _, _ = process_text(input_text)
+        st.write("Prediksi Tingkat Abusiveness:", prediction)
+
+# Tombol "Detect"
+if col3.button("Detect"):
+    if input_text:
+        _, detection, _ = process_text(input_text)
+        st.write("Deteksi Kata Kasar:", detection)
+
+# Tombol "Filter"
+if col4.button("Filter"):
+    if input_text:
+        _, _, filtered_text = process_text(input_text)
         st.write("Teks Setelah Difilter:", filtered_text)
