6891bac3ff916e8813573a27e37f9b1fcca5e669
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-11-01 11:16:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,24 +39,24 @@ col4.markdown(
 if col1.button("Submit"):
     if input_text:
         prediction, detection, filtered_text = process_text(input_text)
-        st.write("Abusiveness Level:", prediction)
-        st.write("Abusive Words Detected:", detection)
-        st.write("Abusive Words Filtered:", filtered_text)
+        st.info("Abusiveness Level:", prediction)
+        st.info("Abusive Words Detected:", detection)
+        st.info("Abusive Words Filtered:", filtered_text)
 
 # Tombol "Predict"
 if col2.button("Predict"):
     if input_text:
         prediction, _, _ = process_text(input_text)
-        st.write("Abusiveness Level:", prediction)
+        st.info("Abusiveness Level:", prediction)
 
 # Tombol "Detect"
 if col3.button("Detect"):
     if input_text:
         _, detection, _ = process_text(input_text)
-        st.write("Abusive Words Detected:", detection)
+        st.info("Abusive Words Detected:", detection)
 
 # Tombol "Filter"
 if col4.button("Filter"):
     if input_text:
         _, _, filtered_text = process_text(input_text)
-        st.write("Abusive Words Filtered:", filtered_text)
+        st.info("Abusive Words Filtered:", filtered_text)
