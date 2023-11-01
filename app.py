967d0f1402ac3a8a4d27b5eedcc326c07465fa93
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-11-01 09:40:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,31 +15,31 @@ st.title("Abusify Streamlit App")
 # Input teks
 input_text = st.text_area("Masukkan teks:")
 
-# Membuat kolom dengan layout horizontal
+# Membuat tiga kolom untuk menampilkan tombol secara horizontal
 col1, col2, col3, col4 = st.columns(4)
 
 # Tombol "Predict"
-if col1.button("Predict"):
+if col2.button("Predict"):
     if input_text:
         prediction, _, _ = process_text(input_text)
-        col1.write("Prediksi Tingkat Abusiveness:", prediction)
+        st.write("Prediksi Tingkat Abusiveness:", prediction)
 
 # Tombol "Detect"
-if col2.button("Detect"):
+if col3.button("Detect"):
     if input_text:
         _, detection, _ = process_text(input_text)
-        col2.write("Deteksi Kata Kasar:", detection)
+        st.write("Deteksi Kata Kasar:", detection)
 
 # Tombol "Filter"
-if col3.button("Filter"):
+if col4.button("Filter"):
     if input_text:
         _, _, filtered_text = process_text(input_text)
-        col3.write("Teks Setelah Difilter:", filtered_text)
+        st.write("Teks Setelah Difilter:", filtered_text)
 
 # Tombol "Submit" untuk menampilkan output semua fungsi
-if col4.button("Submit"):
+if col1.button("Submit"):
     if input_text:
         prediction, detection, filtered_text = process_text(input_text)
-        col4.write("Prediksi Tingkat Abusiveness:", prediction)
-        col4.write("Deteksi Kata Kasar:", detection)
-        col4.write("Teks Setelah Difilter:", filtered_text)
+        st.write("Prediksi Tingkat Abusiveness:", prediction)
+        st.write("Deteksi Kata Kasar:", detection)
+        st.write("Teks Setelah Difilter:", filtered_text)
