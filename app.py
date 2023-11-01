e9795ee3f08970ca0290450a2ceb684df34de1d6
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-11-01 10:52:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ col4.markdown(
 if col1.button("Submit"):
     if input_text:
         prediction, detection, filtered_text = process_text(input_text)
-        st.info("Prediksi Tingkat Abusiveness:", prediction)
-        st.info("Deteksi Kata Kasar:", detection)
-        st.info("Teks Setelah Difilter:", filtered_text)
+        st.text("Prediksi Tingkat Abusiveness:", prediction)
+        st.text("Deteksi Kata Kasar:", detection)
+        st.text("Teks Setelah Difilter:", filtered_text)
 
 # Tombol "Predict"
 if col2.button("Predict"):
