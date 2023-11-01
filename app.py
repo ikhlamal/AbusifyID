d38955164902fc0fbb318640edc812c9af217d99
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-11-01 10:50:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ def process_text(text):
     return prediction, detection, filtered_text
 
 # Aplikasi Streamlit
-st.title("AbusifyID")
-st.write("Abusiveness Verification in Bahasa Indonesia.")
+st.title("Abusify Streamlit App")
 
 # Input teks
-input_text = st.text_area("Input some text than choose any methods below", placeholder="Input text here")
+input_text = st.text_area("Masukkan teks:")
 
 # Membuat tiga kolom dengan CSS untuk mengatur tampilan tombol-tombol
 col1, col2, col3, col4 = st.columns(4)
@@ -39,24 +38,24 @@ col4.markdown(
 if col1.button("Submit"):
     if input_text:
         prediction, detection, filtered_text = process_text(input_text)
-        st.text("Abusiveness Level:", prediction)
-        st.text("Abusive Words Detected:", detection)
-        st.text("Abusive Words Filtered:", filtered_text)
+        st.write("Prediksi Tingkat Abusiveness:", prediction)
+        st.write("Deteksi Kata Kasar:", detection)
+        st.write("Teks Setelah Difilter:", filtered_text)
 
 # Tombol "Predict"
 if col2.button("Predict"):
     if input_text:
         prediction, _, _ = process_text(input_text)
-        st.info("Abusiveness Level:", prediction)
+        st.write("Prediksi Tingkat Abusiveness:", prediction)
 
 # Tombol "Detect"
 if col3.button("Detect"):
     if input_text:
         _, detection, _ = process_text(input_text)
-        st.info("Abusive Words Detected:", detection)
+        st.write("Deteksi Kata Kasar:", detection)
 
 # Tombol "Filter"
 if col4.button("Filter"):
     if input_text:
         _, _, filtered_text = process_text(input_text)
-        st.info("Abusive Words Filtered:", filtered_text)
+        st.write("Teks Setelah Difilter:", filtered_text)
